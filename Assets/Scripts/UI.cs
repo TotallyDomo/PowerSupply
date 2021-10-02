2453f59b4fc4e2e90998809af28c0b34fa83333e
Domas Lukoševičius <domas.lukosevicius@sneakybox.lt>
2021-10-02 18:30:28 +0300

    basic main menu

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -1,12 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UI : MonoBehaviour
 {
+    const string SOURCE_URL = "https://github.com/TotallyDomo/PowerSupply";
+
     void Awake()
     {
         DontDestroyOnLoad(gameObject);
     }
+
+    public void LoadCoreScene() => SceneManager.LoadScene(1);
+
+    public void LoadMainMenu() => SceneManager.LoadScene(0);
+
+    public void OnSourceClick() => Application.OpenURL(SOURCE_URL);
 
 }
