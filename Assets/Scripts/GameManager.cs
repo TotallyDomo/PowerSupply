d2ca2fd982af2a568cd0e5cb87b5d57fbdfca229
DOMO <Domaslu98@gmail.com>
2021-10-03 13:38:29 +0300

    Working UI and game over conditions

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -9,26 +10,36 @@ public class GameManager : MonoBehaviour
     float currentPower;
     [SerializeField]
     float minPower, MaxPower;
+    [SerializeField]
+    Slider powerSlider;
 
     [SerializeField, Header("Heat")]
     float currentHeat;
     [SerializeField]
     float MaxHeat;
+    [SerializeField]
+    Slider heatSlider;
 
     [SerializeField, Header("Waste")]
     float currentWaste;
     [SerializeField]
     float MaxWaste;
+    [SerializeField]
+    Slider wasteSlider;
+
 
     public static Action<float> UpdatePower;
-    public static Action<float> UpdateHeat;
+    public static Action<int> UpdateHeat;
+    public static Action<int> UpdateWaste;
 
     void Awake()
     {
         UpdatePower = null;
         UpdateHeat = null;
+        UpdateWaste = null;
         UpdatePower += OnUpdatePower;
         UpdateHeat += OnUpdateHeat;
+        UpdateWaste += OnUpdateWaste;
     }
 
     public void OnUpdatePower(float amount)
@@ -44,15 +55,30 @@ public class GameManager : MonoBehaviour
             UI.GameOver("Reactor produced too little power, which caused a power outage. As the lead engineer of the power plant, you are fired!");
         }
 
+        powerSlider.value = amount;
+        heatSlider.value = amount;
     }
 
-    void OnUpdateHeat(float amount)
+    void OnUpdateHeat(int amount)
     {
         currentHeat = amount;
+        heatSlider.value = amount;
 
         if (currentHeat > MaxHeat)
         {
             UI.GameOver("Reactor got overheated and melted down! All hope is lost.");
+        }
+
+    }
+
+    void OnUpdateWaste(int amount)
+    {
+        currentWaste = amount;
+        wasteSlider.value = amount;
+
+        if (currentWaste > MaxWaste)
+        {
+            UI.GameOver("The safety comitee has closed the reactor due to overwhelming amount of radioactive waste! You are now unemployed.");
         }
     }
 
